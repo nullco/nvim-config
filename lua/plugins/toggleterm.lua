facea639683d0d7c262b255d21b1e22a8f19963d
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-03-26 21:55:40 +0100

    auto set virtualenv in toggleterm term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,6 +7,13 @@ return {
       open_mapping = [[<C-\>]],
       shade_terminals = false,
       direction = 'float',
+      on_create = function(term)
+        if vim.env.VIRTUAL_ENV == nil then
+          return
+        end
+        term:send('source ' .. vim.env.VIRTUAL_ENV .. '/bin/activate')
+        term:send 'clear'
+      end,
     }
   end,
 }
