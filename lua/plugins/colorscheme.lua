2a3c631357bd6be6f2bfc48a0dda8831f993d85c
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-04-10 23:12:06 +0200

    change to catpuccin theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,37 @@
 return {
-  'dracula/vim',
-  name = 'dracula',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
+  'catppuccin/nvim',
+  lazy = true,
+  priority = 1000,
+  name = 'catppuccin',
+  opts = {
+    integrations = {
+      alpha = true,
+      cmp = true,
+      gitsigns = true,
+      mason = true,
+      mini = true,
+      native_lsp = {
+        enabled = true,
+        underlines = {
+          errors = { 'undercurl' },
+          hints = { 'undercurl' },
+          warnings = { 'undercurl' },
+          information = { 'undercurl' },
+        },
+      },
+      dap = true,
+      dap_ui = true,
+      nvimtree = true,
+      noice = true,
+      notify = true,
+      semantic_tokens = true,
+      telescope = true,
+      treesitter = true,
+      treesitter_context = true,
+      which_key = true,
+    },
+  },
   init = function()
-    vim.cmd.colorscheme 'dracula'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+    vim.cmd.colorscheme 'catppuccin'
   end,
 }
