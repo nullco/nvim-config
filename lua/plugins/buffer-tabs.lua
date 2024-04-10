b5e51e54b82d419cb1aefaa5cec82a885f3ee8d1
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-04-10 09:25:21 +0200

    configure buffer tabs

--- lua/plugins/buffer-tabs.lua
+++ lua/plugins/buffer-tabs.lua
@@ -15,7 +15,15 @@ return {
   opts = {
     options = {
       diagnostics = 'nvim_lsp',
-      always_show_bufferline = false,
+      always_show_bufferline = true,
+      offsets = {
+        {
+          filetype = 'NvimTree',
+          text = 'File Explorer',
+          text_align = 'left',
+          separator = true,
+        },
+      },
     },
   },
 }
