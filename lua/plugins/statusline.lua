2844bc195e22f728d54b1715612b7180c2f79608
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-04-16 17:17:17 +0200

    let status line with defaults

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -5,22 +5,6 @@ return {
     require('lualine').setup {
       options = {
         theme = 'catppuccin',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = { 'NvimTree' },
-      },
-      sections = {
-        lualine_b = { 'branch' },
-        lualine_c = {
-          {
-            'buffers',
-            symbols = {
-              modified = ' ●',
-              alternate_file = '',
-              directory = '',
-            },
-          },
-        },
       },
     }
   end,
