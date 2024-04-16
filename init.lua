86f84c8a1f25226c92cdf079795abbb042df7c84
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-04-16 17:19:44 +0200

    Re-enable netrw

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
--- disable netrw in favor of nvim tree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
