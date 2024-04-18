2683f1cf3222a5f911bb03499ab69748a2f99453
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-04-18 10:13:55 +0200

    cosmetic adjustments

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,37 +1,11 @@
 return {
-  'catppuccin/nvim',
-  lazy = true,
-  priority = 1000,
-  name = 'catppuccin',
-  opts = {
-    integrations = {
-      alpha = true,
-      cmp = true,
-      gitsigns = true,
-      mason = true,
-      mini = true,
-      native_lsp = {
-        enabled = true,
-        underlines = {
-          errors = { 'undercurl' },
-          hints = { 'undercurl' },
-          warnings = { 'undercurl' },
-          information = { 'undercurl' },
-        },
-      },
-      dap = true,
-      dap_ui = true,
-      nvimtree = true,
-      noice = true,
-      notify = true,
-      semantic_tokens = true,
-      telescope = true,
-      treesitter = true,
-      treesitter_context = true,
-      which_key = true,
-    },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    init = function()
+      vim.cmd.colorscheme 'tokyonight'
+    end,
   },
-  init = function()
-    vim.cmd.colorscheme 'catppuccin'
-  end,
 }
--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -18,9 +18,10 @@ return {
 
     -- Set menu
     dashboard.section.buttons.val = {
+      dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
       dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
-      dashboard.button('s', '  > Settings', ':e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>'),
-      dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
+      dashboard.button('s', '  > Settings', ':e $MYVIMRC | :cd %:p:h<CR>'),
+      dashboard.button('q', '󰅚 > Quit NVIM', ':qa<CR>'),
     }
 
     -- Send config to alpha
