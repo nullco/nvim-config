1ff1159417d9b873fd06081a29a3c0e65625280e
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-04-22 10:51:49 +0200

    assume nerdfont is there

--- init.lua
+++ init.lua
@@ -4,13 +4,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = true
-
 require 'config.options'
 require 'config.keymaps'
 require 'config.autocommands'
 require 'config.lazy'
 
--- The line beneath this is called `modeline`. See `:help modeline`
+-- See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
