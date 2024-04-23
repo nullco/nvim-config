e32631ac94467271d762855f62e383a9d29718ad
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-04-23 21:10:12 +0200

    remove comments

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,10 +1,4 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
+-- Configures everything related to debug
 
 return {
   -- NOTE: Yes, you can install new plugins here!
