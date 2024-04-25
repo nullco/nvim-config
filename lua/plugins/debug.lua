1881300efb9b976c90aeeb34813d63b16f217533
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-04-25 15:00:57 +0200

    remove dap ui

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -3,15 +3,12 @@
 return {
   'mfussenegger/nvim-dap',
   dependencies = {
-    'rcarriga/nvim-dap-ui',
-    'nvim-neotest/nvim-nio',
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
     'mfussenegger/nvim-dap-python',
   },
   config = function()
     local dap = require 'dap'
-    local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
       automatic_setup = true,
@@ -21,35 +18,19 @@ return {
       },
     }
 
-    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F6>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F7>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F8>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<Leader>dt', dap.repl.toggle, { desc = 'Debug: Toggle REPL' })
+    vim.keymap.set('n', '<Leader>dx', dap.terminate, { desc = 'Debug: Disconnect' })
+    vim.keymap.set('n', '<Leader>dr', dap.restart, { desc = 'Debug: Restart' })
     vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle [B]reakpoint' })
     vim.keymap.set('n', '<leader>dB', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end, { desc = 'Debug: Set Conditional [B]reakpoint' })
+    vim.keymap.set('n', '<leader>dd', dap.clear_breakpoints, { desc = 'Debug: Delete All Breakpoints' })
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      layouts = {
-        {
-          elements = { {
-            id = 'repl',
-            size = 1.0,
-          } },
-          position = 'bottom',
-          size = 10,
-        },
-      },
-    }
-
-    vim.keymap.set('n', '<Leader>dt', dapui.toggle, { desc = 'Debug: See last session result.' })
-
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+    vim.keymap.set('n', '<F6>', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<F7>', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<F8>', dap.step_out, { desc = 'Debug: Step Out' })
 
     require('dap-python').setup '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
     require('dap.ext.vscode').load_launchjs(nil, { debugpy = { 'python' } })
