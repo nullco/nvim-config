3acd947960c0dad78dfb807eeebba28449486e52
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-05-06 22:51:53 +0200

    add buffer keywords source

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -34,6 +34,7 @@ return {
     --  into multiple repos for maintenance purposes.
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
+    'hrsh7th/cmp-buffer',
   },
   config = function()
     -- See `:help cmp`
@@ -96,6 +97,7 @@ return {
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
       sources = {
+        { name = 'buffer' },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'path' },
