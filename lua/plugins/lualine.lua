38733e1339bccef38063e54726318bcee5f0d594
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-05-08 23:59:56 +0200

    extra config for status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,19 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup {}
+    require('lualine').setup {
+      options = {
+        globalstatus = true
+      },
+      sections = {
+        lualine_c = {}
+      },
+      inactive_winbar = {
+        lualine_c = {{"filename", filestatus = true, path = 1}}
+      },
+      winbar = {
+        lualine_c = {{"filename", filestatus = true, path = 1}}
+      }
+    }
   end,
 }
