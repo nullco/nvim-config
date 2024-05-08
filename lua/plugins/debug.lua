e9e1e5f3858948482091b54360690ba1fb8b3947
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-05-08 22:09:57 +0200

    remap keys for debugging and use tabnew for dap-terminal

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -19,7 +19,7 @@ return {
       },
     }
 
-    vim.keymap.set('n', '<Leader>dt', dap.repl.toggle, { desc = 'Debug: Toggle REPL' })
+    vim.keymap.set('n', '<Leader>dtr', dap.repl.toggle, { desc = 'Debug: [T]oggle REPL' })
     vim.keymap.set('n', '<Leader>dx', dap.terminate, { desc = 'Debug: Disconnect' })
     vim.keymap.set('n', '<Leader>dr', dap.restart, { desc = 'Debug: Restart' })
     vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle [B]reakpoint' })
@@ -32,12 +32,16 @@ return {
     vim.keymap.set('n', '<F6>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F7>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F8>', dap.step_out, { desc = 'Debug: Step Out' })
+  
+
+    -- Setup DAP
+    dap.defaults.fallback.terminal_win_cmd = 'tabnew'
 
     -- Setup DAP for python
     dap_python.setup '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
     dap_python.test_runner = 'pytest'
-    vim.keymap.set('n', '<Leader>dm', dap_python.test_method, { desc = 'Debug: Test Current [M]ethod' })
-    vim.keymap.set('n', '<Leader>dc', dap_python.test_class, { desc = 'Debug: Test Current [C]lass' })
+    vim.keymap.set('n', '<Leader>dtm', dap_python.test_method, { desc = 'Debug: Test Current [M]ethod' })
+    vim.keymap.set('n', '<Leader>dtc', dap_python.test_class, { desc = 'Debug: Test Current [C]lass' })
 
     -- Setup launch.json from vscode to support entry points per project in ./vscode/launch.json
     require('dap.ext.vscode').load_launchjs(nil, { debugpy = { 'python' } })
