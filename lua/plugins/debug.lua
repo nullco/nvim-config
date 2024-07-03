08001f358ffc1a2577e3bcc5dad272a6fd29f367
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2024-07-03 14:19:14 +0200

    add debug icons

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -10,9 +10,20 @@ return {
     'mfussenegger/nvim-dap-python'
   },
   config = function()
+    -- vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
+    -- vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
+    -- vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
+
+    vim.fn.sign_define('DapBreakpoint', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+    vim.fn.sign_define('DapBreakpointCondition', { text='󰯲', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+    vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
+    vim.fn.sign_define('DapLogPoint', { text='󰰍', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
+    vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
+
     local dap = require 'dap'
     local dapui = require 'dapui'
     local dap_python = require 'dap-python'
+
 
     require('mason-nvim-dap').setup {
       automatic_setup = true,
