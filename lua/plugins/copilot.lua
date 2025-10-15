5dc01e3fb19bce3413d5087800d5d62ca15883a9
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2025-10-15 22:38:49 +0200

    tweak copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,11 +1,11 @@
 return {
   "github/copilot.vim",
   config = function()
-    vim.g.copilot_no_tab_map = true
-    vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', {
+    vim.keymap.set("i", "<C-j>", 'copilot#Accept("")', {
       expr = true,
       replace_keycodes = false,
       desc = "Accept Copilot suggestion",
     })
+    vim.g.copilot_no_tab_map = true
   end,
 }
