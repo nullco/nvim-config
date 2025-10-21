881e593ecf594f66b98ec7a9e1dc30e6d68d690d
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2025-10-21 19:51:25 +0200

    fix: lsp throwing error on textDocument/documentHighlight

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,34 +1,40 @@
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('nullco-lsp-attach', { clear = true }),
   callback = function(event)
-  local map = function(keys, func, desc)
-  vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-  end
+    local map = function(keys, func, desc)
+      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
 
-  map("<leader>lr", ":LspRestart<CR>", 'Restart server')
-  map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  map('K', vim.lsp.buf.hover, 'Hover Documentation')
-  map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-  local client = vim.lsp.get_client_by_id(event.data.client_id)
-  if client and client.server_capabilities.documentHighlightProvider then
-    -- Highlight references of the word under cursor.
-    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-      buffer = event.buf,
-      callback = vim.lsp.buf.document_highlight,
-    })
-
-    -- Clear highlights when cursor moved.
-    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-      buffer = event.buf,
-      callback = vim.lsp.buf.clear_references,
-    })
-  end
+    map("<leader>lr", ":LspRestart<CR>", 'Restart server')
+    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+    map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    map('K', vim.lsp.buf.hover, 'Hover Documentation')
+    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+      local highlight_augroup = vim.api.nvim_create_augroup("nullco-lsp-highlight", { clear = false })
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+      vim.api.nvim_create_autocmd("LspDetach", {
+        group = vim.api.nvim_create_augroup("nullco-lsp-detach", { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds({ group = "nullco-lsp-highlight", buffer = event2.buf })
+        end,
+      })
+    end
   end,
 })
 
