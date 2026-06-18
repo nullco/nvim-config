1c0fb61877e2007d0026bed54b852e6d51b3fe32
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2026-06-18 23:44:30 +0200

    keymaps adjustments for buffer deletion

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,8 +21,28 @@ vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
 vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
 
 -- Buffer management
-vim.keymap.set('n', '<Leader>bd', [[:%bd!<CR>]], { desc = 'Close all buffers' })
-vim.keymap.set('n', '<Leader>bo', [[:%bd | e# | bd#<CR> | ']], { desc = 'Close all buffers but current one' })
+vim.keymap.set('n', '<Leader>bd', function()
+  local cur = vim.api.nvim_get_current_buf()
+  -- reuse an existing unnamed buffer if one exists (even if modified)
+  local target
+  for _, b in ipairs(vim.api.nvim_list_bufs()) do
+    if b ~= cur and vim.bo[b].buflisted and vim.api.nvim_buf_get_name(b) == '' then
+      target = b
+      break
+    end
+  end
+  if target then
+    vim.cmd('buffer ' .. target)
+  else
+    vim.cmd('enew')
+  end
+  -- the buffer may have auto-wiped on switch-away (e.g. fugitive's bufhidden=wipe)
+  if vim.api.nvim_buf_is_valid(cur) then
+    pcall(vim.cmd, 'bd ' .. cur)
+  end
+end, { desc = 'Close buffer (keep window)' })
+vim.keymap.set('n', '<Leader>bD', [[:%bd!<CR>]], { desc = 'Close all buffers' })
+vim.keymap.set('n', '<Leader>bo', [[:%bd | e# | bd#<CR>]], { desc = 'Close all buffers but current one' })
 vim.keymap.set('n', '<S-h>', ':tabp<CR>', { desc = 'Prev tab' })
 vim.keymap.set('n', '<S-l>', ':tabn<CR>', { desc = 'Next tab' })
 
@@ -36,4 +56,3 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Windows: Move focus one up'
 vim.keymap.set('n', '<Leader>wl', '<C-w>v', { desc = 'Windows: split vertically' })
 vim.keymap.set('n', '<Leader>wj', '<C-w>s', { desc = 'Windows: split horizontally' })
 vim.keymap.set('n', '<Leader>wo', ':only<CR>', { desc = 'Windows: leave [o]nly current window open' })
-vim.keymap.set('n', '<Leader>wbd', [[:bp | bd#<CR>]], { desc = 'Windows: Close current window buffer without closing window' })
