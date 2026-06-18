2037cd03f5b69a689ca5439f8d7e08797b72af0d
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2026-06-18 23:57:25 +0200

    do not show fugitive / term buffers in standard buffer search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -29,6 +29,82 @@ return {
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
+
+    -- Custom buffer picker. builtin.buffers has no name/buftype filter, so we
+    -- mirror it here and add an `opts.keep(bufnr)` predicate. Reuses the same
+    -- MRU sort, grep previewer, generic sorter and <M-d> delete-buffer mapping.
+    local pickers = require 'telescope.pickers'
+    local finders = require 'telescope.finders'
+    local make_entry = require 'telescope.make_entry'
+    local conf = require('telescope.config').values
+    local actions = require 'telescope.actions'
+
+    local function is_fugitive(bufnr)
+      return vim.api.nvim_buf_get_name(bufnr):match '^fugitive://' ~= nil
+    end
+
+    local function is_terminal(bufnr)
+      return vim.bo[bufnr].buftype == 'terminal'
+    end
+
+    local function pick_buffers(opts)
+      opts = opts or {}
+      opts.prompt_title = opts.prompt_title or 'Buffers'
+      local keep = opts.keep or function(_) return true end
+
+      local bufnrs = vim.tbl_filter(function(bufnr)
+        if vim.fn.buflisted(bufnr) ~= 1 then
+          return false
+        end
+        if opts.show_all_buffers == false and not vim.api.nvim_buf_is_loaded(bufnr) then
+          return false
+        end
+        if opts.ignore_current_buffer and bufnr == vim.api.nvim_get_current_buf() then
+          return false
+        end
+        return keep(bufnr)
+      end, vim.api.nvim_list_bufs())
+
+      if not next(bufnrs) then
+        vim.notify(opts.empty_msg or 'No buffers found', vim.log.levels.INFO)
+        return
+      end
+
+      if opts.sort_mru then
+        table.sort(bufnrs, function(a, b)
+          return vim.fn.getbufinfo(a)[1].lastused > vim.fn.getbufinfo(b)[1].lastused
+        end)
+      end
+
+      local buffers = {}
+      for _, bufnr in ipairs(bufnrs) do
+        local flag = bufnr == vim.fn.bufnr '' and '%' or (bufnr == vim.fn.bufnr '#' and '#' or ' ')
+        table.insert(buffers, {
+          bufnr = bufnr,
+          flag = flag,
+          info = vim.fn.getbufinfo(bufnr)[1],
+        })
+      end
+
+      opts.bufnr_width = opts.bufnr_width or #tostring(math.max(unpack(bufnrs)))
+
+      pickers
+        .new(opts, {
+          finder = finders.new_table {
+            results = buffers,
+            entry_maker = opts.entry_maker or make_entry.gen_from_buffer(opts),
+          },
+          previewer = conf.grep_previewer(opts),
+          sorter = conf.generic_sorter(opts),
+          default_selection_index = 1,
+          attach_mappings = function(_, map)
+            map({ 'i', 'n' }, '<M-d>', actions.delete_buffer)
+            return true
+          end,
+        })
+        :find()
+    end
+
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
@@ -38,11 +114,27 @@ return {
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', function ()
-      builtin.buffers {
-        sort_mru = true
+    -- Source buffers only: drop fugitive:// buffers entirely and keep :term
+    -- buffers in their own picker so src buffers stay clean.
+    vim.keymap.set('n', '<leader><leader>', function()
+      pick_buffers {
+        sort_mru = true,
+        prompt_title = 'Buffers (src)',
+        empty_msg = 'No source buffers',
+        keep = function(bufnr)
+          return not is_fugitive(bufnr) and not is_terminal(bufnr)
+        end,
       }
-    end, { desc = '[ ] Find existing buffers' })
+    end, { desc = '[ ] Find existing src buffers' })
+
+    vim.keymap.set('n', '<leader>st', function()
+      pick_buffers {
+        sort_mru = true,
+        prompt_title = 'Buffers (term)',
+        empty_msg = 'No terminal buffers',
+        keep = is_terminal,
+      }
+    end, { desc = '[S]earch [T]erm buffers' })
     vim.keymap.set('n', '<leader>sb', builtin.git_branches, { desc = '[S]earch [B]ranches' })
     vim.keymap.set('n', '<leader>sc', builtin.git_commits, { desc = '[S]earch [C]ommits' })
 
